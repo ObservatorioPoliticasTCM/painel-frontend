b45d325f41d8c6a899cbb438cd94b9cf8f86804b
Bruno Azuma Balzano <41346451+brunoazuma@users.noreply.github.com>
2025-05-30 17:06:05 -0300

    6 adicionar scroll snap (#10)
    * Adicionada funcionalidade de scroll-snap
    
    * Adicionado componente de capa inicial
    
    * Capa adicionada a home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,17 @@
 <template>
   <main class="home-view">
-    <AppHeader />
-    <HomeHero />
-    <CardGrid />
-    <DashboardFrame appid="13e05213-9609-45cd-86df-5cbf75836d79"
-                    sheet="6afd5fd9-e62b-45d8-a4e9-808bf98d534d"
-                    id="dashboard-home" />
+    <div class="snap-container">
+      <FrontCover />
+      <CardGrid />
+      <DashboardFrame appid="13e05213-9609-45cd-86df-5cbf75836d79"
+                      sheet="6afd5fd9-e62b-45d8-a4e9-808bf98d534d"
+                      id="dashboard-home" />
+    </div>
   </main>
 </template>
 
 <script setup lang="ts">
-import AppHeader from '../components/AppHeader.vue'
-import HomeHero from '../components/HomeHero.vue'
+import FrontCover from '../components/FrontCover.vue'
 import CardGrid from '../components/CardGrid.vue'
 import DashboardFrame from '../components/DashboardFrame.vue'
 </script>
@@ -26,5 +26,17 @@ import DashboardFrame from '../components/DashboardFrame.vue'
 #dashboard-home {
   position: relative;
   z-index: 30;
+}
+
+/* Container de scroll com snap */
+.snap-container {
+  height: 100vh;
+  overflow-y: scroll;
+  scroll-snap-type: y mandatory;
+}
+
+/* Cada componente dentro do container será um ponto de travamento */
+.snap-container > * {
+  scroll-snap-align: start;
 }
 </style>
