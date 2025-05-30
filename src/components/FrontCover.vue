8b25dc3df75c06415f9f1ae6c26b55d88b5c36ec
Bruno Azuma Balzano <brunoazuma@usp.br>
2025-05-30 16:44:39 -0300

    Adicionado componente de capa inicial

---
+++ src/components/FrontCover.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="front-cover">
+    <AppHeader />
+    <HomeHero />
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppHeader from './AppHeader.vue'
+import HomeHero from './HomeHero.vue'
+</script>
+
+<style scoped>
+</style>
