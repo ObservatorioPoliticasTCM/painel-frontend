732989aa0fd8d74e70d3483a678fab375c0c414f
Bruno Azuma Balzano <41346451+brunoazuma@users.noreply.github.com>
2025-05-30 16:28:47 -0300

    2 adicionar grid de cards (#5)
    * Criados componentes de card e cardgrid
    
    * Card grid adicionado a home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,14 @@
   <main class="home-view">
     <AppHeader />
     <HomeHero />
+    <CardGrid />
   </main>
 </template>
 
 <script setup lang="ts">
 import AppHeader from '../components/AppHeader.vue'
 import HomeHero from '../components/HomeHero.vue'
+import CardGrid from '../components/CardGrid.vue'
 </script>
 
 <style scoped>
