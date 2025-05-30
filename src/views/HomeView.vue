8599700562067781853ec82439b07ee0a58d40cb
Bruno Azuma Balzano <41346451+brunoazuma@users.noreply.github.com>
2025-05-30 16:38:31 -0300

    3 adicionar componente de dashboards (#9)
    * Adicionado componente de dashboards
    
    * Dashboard adicionado a home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,9 @@
     <AppHeader />
     <HomeHero />
     <CardGrid />
+    <DashboardFrame appid="13e05213-9609-45cd-86df-5cbf75836d79"
+                    sheet="6afd5fd9-e62b-45d8-a4e9-808bf98d534d"
+                    id="dashboard-home" />
   </main>
 </template>
 
@@ -10,6 +13,7 @@
 import AppHeader from '../components/AppHeader.vue'
 import HomeHero from '../components/HomeHero.vue'
 import CardGrid from '../components/CardGrid.vue'
+import DashboardFrame from '../components/DashboardFrame.vue'
 </script>
 
 <style scoped>
@@ -17,5 +21,10 @@ import CardGrid from '../components/CardGrid.vue'
   min-height: 100vh;
   overflow: hidden;
   background-color: #121212; /* fundo escuro */
+}
+
+#dashboard-home {
+  position: relative;
+  z-index: 30;
 }
 </style>
