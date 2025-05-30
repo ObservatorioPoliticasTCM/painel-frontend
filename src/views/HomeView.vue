ff2ba98eea2e91209621644375131d1d2b8d9d50
Bruno Azuma Balzano <brunoazuma@usp.br>
2025-05-30 16:44:02 -0300

    Adicionada funcionalidade de scroll-snap

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
   <main class="home-view">
-    <AppHeader />
-    <HomeHero />
-    <CardGrid />
-    <DashboardFrame appid="13e05213-9609-45cd-86df-5cbf75836d79"
-                    sheet="6afd5fd9-e62b-45d8-a4e9-808bf98d534d"
-                    id="dashboard-home" />
+    <div class="snap-container">
+      <AppHeader />
+      <HomeHero />
+      <CardGrid />
+      <DashboardFrame appid="13e05213-9609-45cd-86df-5cbf75836d79"
+                      sheet="6afd5fd9-e62b-45d8-a4e9-808bf98d534d"
+                      id="dashboard-home" />
+    </div>
   </main>
 </template>
 
@@ -26,5 +28,17 @@ import DashboardFrame from '../components/DashboardFrame.vue'
 #dashboard-home {
   position: relative;
   z-index: 30;
+}
+
+/* Container de scroll com snap */
+.snap-container {
+  height: 100vh;
+  overflow-y: scroll;
+  scroll-snap-type: y mandatory;
+}
+
+/* Cada componente dentro do container será um ponto de travamento */
+.snap-container > * {
+  scroll-snap-align: start;
 }
 </style>
