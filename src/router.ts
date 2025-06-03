7134f472dbb3c5c8113e6ac9a44f3653290731cf
Bruno Azuma Balzano <brunoazuma@usp.br>
2025-06-03 17:25:44 -0300

    EducacaoView adicionada

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from './views/HomeView.vue'
+import EducacaoView from './views/EducacaoView.vue'
 
 const routes = [
-  { path: '/', name: 'Home', component: HomeView }
+  { path: '/', name: 'Home', component: HomeView },
+  { path: '/educacao', name: 'educacao', component: EducacaoView }
 ]
 
 const router = createRouter({
---
+++ src/views/EducacaoView.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="educacao-view">
+    <div class="snap-container">
+        <DashboardFrame
+        appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
+        sheet="nvgxg"
+        />
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import DashboardFrame from '../components/DashboardFrame.vue'
+</script>
+
+<style scoped>
+.educacao-view {
+  min-height: 100vh;
+  background-color: #121212;
+  overflow: hidden;
+}
+</style>
