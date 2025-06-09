90930d0b1975ad37547b713de3adc85c83465260
Bruno Azuma Balzano <brunoazuma@usp.br>
2025-06-09 15:02:28 -0300

    Adicionados títulos aos dashboards da educacao

--- src/views/EducacaoView.vue
+++ src/views/EducacaoView.vue
@@ -5,21 +5,26 @@
       <DashboardFrame
       appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
       sheet="8a9478a1-23b6-4165-a526-12f9d736aaa0"
+      title="Número de unidades escolares"
+      subtitle="Inclui as unidades escolares particulares"
       identity="1"
       />
       <DashboardFrame
       appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
       sheet="nNpyj"
+      title="Perfil dos educandos da rede pública municipal"
       identity="2"
       />
       <DashboardFrame
       appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
       sheet="nvgxg"
+      title="Perfil dos deducadores da rede pública municipal"
       identity="3"
       />
       <DashboardFrame
       appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
       sheet="PqxZb"
+      title="Educandos estrangeiros na rede pública municipal"
       identity="4"
       />
     </SnapContainer>
