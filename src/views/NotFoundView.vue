88eb96452fa82c601f8ba5966f2f6dd6533d775a
Bruno Azuma Balzano <brunoazuma@usp.br>
2025-06-09 14:32:40 -0300

    Adicionada página de erro 404

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,26 @@
+<!-- filepath: /c:/Users/855395/Git/painel-frontend/src/views/NotFound.vue -->
+<template>
+  <Background />
+  <AppHeader />
+  <NotFoundHero />
+</template>
+  
+<script setup lang="ts">
+import AppHeader from '../components/AppHeader.vue';
+import Background from '../components/Background.vue';
+import NotFoundHero from '../components/NotFoundHero.vue';
+
+// Use este espaço para lógica, se necessário.
+</script>
+
+<style scoped>
+.not-found {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #121212;
+  color: white;
+}
+</style>
