f3b8e60f18bedec708266aaa41065df5f3e1f637
Bruno Azuma Balzano <41346451+brunoazuma@users.noreply.github.com>
2025-06-09 14:36:42 -0300

    21 corrigir direcionamento de rota no nginx (#24)
    * Adicionado arquivo de configuração do nginx
    
    * Arquivo adicionado ao dockerfile
    
    * Adicionada página de erro 404
    
    * Adicionada rota genérica pra página não encontrada

---
+++ src/components/NotFoundHero.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="hero-section">
+    <div class="hero-content">
+      <h1 class="hero-title">Página não econtrada</h1>
+      <p class="hero-subtitle">A página que você está procurando não existe.</p>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.hero-section {
+  position: relative;
+  width: 100%;
+  height: 80vh;
+  aspect-ratio: 16 / 9;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  text-align: center;
+  background-color: transparent; /* fallback */
+}
+
+.hero-content {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  z-index: 10; /* acima da overlay e fundo */
+}
+
+.hero-title {
+  font-size: 8rem;
+  margin-bottom: 0;
+  font-weight: 400;
+  color: black;
+  text-transform: uppercase;
+}
+
+.hero-subtitle {
+  font-size: 3.75rem;
+  font-weight: 500;
+  color: black;
+  margin: 0;
+  text-transform: uppercase;
+}
+</style>
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from './views/HomeView.vue'
 import EducacaoView from './views/EducacaoView.vue'
+import NotFound from './views/NotFoundView.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
-  { path: '/educacao', name: 'educacao', component: EducacaoView }
+  { path: '/educacao', name: 'educacao', component: EducacaoView },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound }
 ]
 
 const router = createRouter({
