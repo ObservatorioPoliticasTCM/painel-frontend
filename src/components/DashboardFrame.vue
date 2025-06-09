2c1997c37a593162edda25d121056094eb520690
Bruno Azuma Balzano <brunoazuma@usp.br>
2025-06-09 14:56:08 -0300

    Adicionados elementos de título e subtítulo opcionais no componente

--- src/components/DashboardFrame.vue
+++ src/components/DashboardFrame.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="frame-container">
-    <iframe :src="iframeSrc" frameborder="0" style="border:none;width:100%;height:100%;"></iframe>
+  <div class="dashboard-frame">
+    <div v-if="title || subtitle" class="frame-header">
+      <h1 v-if="title">{{ title }}</h1>
+      <small v-if="subtitle">{{ subtitle }}</small>
+    </div>
+    <div class="iframe-wrapper">
+      <iframe :src="iframeSrc" frameborder="0" style="border:none;width:100%;height:100%;"></iframe>
+    </div>
   </div>
 </template>
 
@@ -11,7 +17,9 @@ export default defineComponent({
   props: {
     appid: { type: String, required: true },
     sheet: { type: String, required: true },
-    identity: { type: String }
+    identity: { type: String },
+    title: { type: String, default: '' },
+    subtitle: { type: String, default: '' }
   },
   setup(props) {
     const baseUrl = "https://qlik.tcm.sp.gov.br/jwt/single/"
@@ -22,16 +30,37 @@ export default defineComponent({
       }
       return url
     })
-    return { iframeSrc }
+    return { iframeSrc, ...props }
   }
 })
 </script>
 
 <style scoped>
-.frame-container {
+.dashboard-frame {
+  display: flex;
+  flex-direction: column;
   height: 90vh;
   padding: 5vh;
-  position: relative;
+  position: relative; /* adiciona posicionamento para que z-index funcione */
   z-index: 20;
+}
+.frame-header {
+  display: flex;
+  align-items: baseline;
+  padding: 1rem;
+}
+.frame-header h1 {
+  margin: 0;
+  text-transform: uppercase;
+  text-align: left;
+  font-weight: normal;
+}
+.frame-header small {
+  font-variant: small-caps;
+  text-align: left;
+  margin-left: 1rem;
+}
+.iframe-wrapper {
+  flex: 1;
 }
 </style>
