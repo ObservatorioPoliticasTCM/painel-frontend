3a042d454d4b003fa7a0051debf425685a0b12ab
Bruno Azuma Balzano <41346451+brunoazuma@users.noreply.github.com>
2025-06-11 18:41:45 -0300

    32 adicionar dashboards do gt saude (#33)
    * Adicionada SaudeView
    
    * Rota adicionada e navbar ajustada

---
+++ src/views/SaudeView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="saude-view">
+    <SnapContainer>
+      <AppHeader  />
+      <DashboardFrame
+      appid="585f0398-5876-4e29-98db-a53e3e013dc4"
+      sheet="EnGpsY"
+      title="Número de consultas em 2023"
+      subtitle="Inclui as unidades municipais, estaduais e federais"
+      identity="1"
+      />
+      <DashboardFrame
+      appid="585f0398-5876-4e29-98db-a53e3e013dc4"
+      sheet="kPfKtR"
+      title="Leitos e mortalidade"
+      subtitle="Inclui leitos das unidades municipais, estaduais e federais"
+      identity="2"
+      />
+      <DashboardFrame
+      appid="585f0398-5876-4e29-98db-a53e3e013dc4"
+      sheet="XFZvFPN"
+      title="Despesas em atenção Básica e especializada"
+      identity="3"
+      />
+    </SnapContainer>
+    <Background />
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppHeader from '../components/AppHeader.vue';
+import Background from '../components/Background.vue';
+import DashboardFrame from '../components/DashboardFrame.vue'
+import SnapContainer from '../components/SnapContainer.vue';
+</script>
+
+<style scoped>
+.saude-view {
+  min-height: 100vh;
+  overflow: hidden;
+}
+</style>
