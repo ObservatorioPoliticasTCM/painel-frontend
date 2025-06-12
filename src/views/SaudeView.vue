8967e0334e9f9ed2cff895923a456a3622443cff
Bruno Azuma Balzano <brunoazuma@usp.br>
2025-06-12 13:12:55 -0300

    SaudeView alterada

--- src/views/SaudeView.vue
+++ src/views/SaudeView.vue
@@ -4,6 +4,13 @@
       <AppHeader  />
       <DashboardFrame
       appid="585f0398-5876-4e29-98db-a53e3e013dc4"
+      sheet="XFZvFPN"
+      title="Despesas em atenção Básica e especializada"
+      subtitle="Inclui apenas unidades municipais"
+      identity="3"
+      />
+      <DashboardFrame
+      appid="585f0398-5876-4e29-98db-a53e3e013dc4"
       sheet="EnGpsY"
       title="Número de consultas em 2023"
       subtitle="Inclui as unidades municipais, estaduais e federais"
@@ -15,13 +22,6 @@
       title="Leitos e mortalidade"
       subtitle="Inclui leitos das unidades municipais, estaduais e federais"
       identity="2"
-      />
-      <DashboardFrame
-      appid="585f0398-5876-4e29-98db-a53e3e013dc4"
-      sheet="XFZvFPN"
-      title="Despesas em atenção Básica e especializada"
-      subtitle="Inclui apenas unidades municipais"
-      identity="3"
       />
     </SnapContainer>
     <Background />
