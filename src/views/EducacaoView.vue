ff31e359eac35def9c21ff6f9dfcace3ca50112c
Bruno Azuma Balzano <41346451+brunoazuma@users.noreply.github.com>
2025-06-12 12:49:53 -0300

    Ajustes para apresentação (#40)

--- src/views/EducacaoView.vue
+++ src/views/EducacaoView.vue
@@ -4,28 +4,34 @@
       <AppHeader  />
       <DashboardFrame
       appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
+      sheet="dRrpWnj"
+      title="Visão geral da educação"
+      identity="1"
+      />
+      <DashboardFrame
+      appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
       sheet="8a9478a1-23b6-4165-a526-12f9d736aaa0"
       title="Número de unidades escolares"
       subtitle="Inclui as unidades escolares particulares"
-      identity="1"
+      identity="2"
       />
       <DashboardFrame
       appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
       sheet="nNpyj"
       title="Perfil dos educandos da rede pública municipal"
-      identity="2"
+      identity="3"
       />
       <DashboardFrame
       appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
       sheet="nvgxg"
       title="Perfil dos educadores da rede pública municipal"
-      identity="3"
+      identity="4"
       />
       <DashboardFrame
       appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
       sheet="PqxZb"
       title="Educandos estrangeiros na rede pública municipal"
-      identity="4"
+      identity="5"
       />
     </SnapContainer>
     <Background />
