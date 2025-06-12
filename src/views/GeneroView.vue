4ef66a3af5184f6336cf573db9628dce07f452ff
Bruno Azuma Balzano <41346451+brunoazuma@users.noreply.github.com>
2025-06-11 21:37:41 -0300

    App e pasta alterados (#38)

--- src/views/GeneroView.vue
+++ src/views/GeneroView.vue
@@ -3,11 +3,10 @@
     <SnapContainer>
       <AppHeader  />
       <DashboardFrame
-      appid="938edf5e-7b67-4bbb-8b2f-5a97ed6f0afd"
-      sheet="93cf2941-83d4-4b8a-bac9-40471776f2fe"
+      appid="4835ca37-8af2-4b04-aea1-57d99911f3f5"
+      sheet="a0e0c147-849d-4ab9-a43d-369e0c1e0af3"
       title="Orçamento aplicado em Ações Sensíveis a Políticas para Mulheres"
       subtitle="Inclui apenas ações voltadas exclusivamente a políticas de combate à violência contra a mulher"
-      select="$::grupoA,E1"
       />
     </SnapContainer>
     <Background />
