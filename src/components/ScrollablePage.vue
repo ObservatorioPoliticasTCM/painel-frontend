b6ae662c166240d49c6d57ad9b5c37fb817e9991
Bruno Azuma Balzano <brunoazuma@usp.br>
2025-08-01 23:11:23 -0300

    Criado componente geral para páginas sem scroll snap

---
+++ src/components/ScrollablePage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="scrollable-page">
+        <AppHeader />
+        <slot />
+        <Footer />
+        <Background />
+    </div>
+</template>
+
+<script setup lang="ts">
+import AppHeader from './AppHeader.vue';
+import Footer from './Footer.vue';
+import Background from './Background.vue';
+</script>
+
+<style scoped>
+.scrollable-page {
+    min-height: 100vh;
+    overflow: hidden;
+}
+</style>
