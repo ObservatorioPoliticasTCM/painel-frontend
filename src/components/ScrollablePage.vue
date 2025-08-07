ba8be6403855f37a5f6b15ecc3a1dd2d2d547e09
Bruno Azuma Balzano <brunoazuma@usp.br>
2025-08-07 20:10:09 -0300

    Seleção de background adicionado a todas as páginas

--- src/components/ScrollablePage.vue
+++ src/components/ScrollablePage.vue
@@ -2,10 +2,17 @@
     <div class="scrollable-page">
         <AppHeader />
         <div class="scrollable-content">
+            <Background :version="backgroundVersion" />
             <slot />
         </div>
         <Footer />
-        <Background />
+        <ToggleSwitch
+            class="design-toggle"
+            title="Altere a versão do design"
+            leftButtonText="Versão com foto"
+            rightButtonText="Versão com marca d'água"
+            v-model="isV2"
+        />
     </div>
 </template>
 
@@ -13,6 +20,10 @@
 import AppHeader from './AppHeader.vue';
 import Footer from './Footer.vue';
 import Background from './Background.vue';
+import ToggleSwitch from './ToggleSwitch.vue';
+import { useDesignVersion } from '../composables/useDesignVersion';
+
+const { isV2, backgroundVersion } = useDesignVersion()
 </script>
 
 <style scoped>
@@ -20,11 +31,20 @@ import Background from './Background.vue';
     min-height: 100vh;
     display: flex;
     flex-direction: column;
+    position: relative;
+    overflow-x: hidden;
 }
 
 .scrollable-content {
     flex: 1 0 auto;
     display: flex;
     flex-direction: column;
+}
+
+.design-toggle {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  z-index: 100;
 }
 </style>
--- src/components/SnapPage.vue
+++ src/components/SnapPage.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="snap-page">
-        <SnapContainer>
+        <SnapContainer class="snap-container">
+            <Background :version="backgroundVersion" />
             <AppHeader />
             <slot />
             <Footer />
         </SnapContainer>
-        <Background />
+        <ToggleSwitch
+            class="design-toggle"
+            title="Altere a versão do design"
+            leftButtonText="Versão com foto"
+            rightButtonText="Versão com marca d'água"
+            v-model="isV2"
+        />
     </div>
 </template>
 
@@ -14,11 +21,26 @@ import SnapContainer from './SnapContainer.vue';
 import AppHeader from './AppHeader.vue';
 import Footer from './Footer.vue';
 import Background from './Background.vue';
+import ToggleSwitch from './ToggleSwitch.vue';
+import { useDesignVersion } from '../composables/useDesignVersion';
+
+const { isV2, backgroundVersion } = useDesignVersion()
 </script>
 
 <style scoped>
 .snap-page {
     min-height: 100vh;
     overflow: hidden;
+}
+
+.snap-container {
+    position: relative;
+}
+
+.design-toggle {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  z-index: 100;
 }
 </style>
