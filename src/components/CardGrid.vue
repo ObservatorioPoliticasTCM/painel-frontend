c3d070aecd01e2014dedd67aadadc4eaf8fffcf3
Bruno Azuma Balzano <41346451+brunoazuma@users.noreply.github.com>
2025-08-16 11:32:00 -0300

    80 atualizar texto dos cards (#87)
    * Adicionado título ao info-cell e tamanho da fonte ajustado
    
    * Ajustado estilo de card-face
    
    * Textos de todos os GTs atualizados

--- src/components/CardGrid.vue
+++ src/components/CardGrid.vue
@@ -2,42 +2,43 @@
     <div class="grid">
       <!-- Célula de informação -->
       <div class="info-cell">
-        <p>Você pode consultar os indicadores específicos de cada tema...</p>
+        <h2>Dados</h2>
+        <p>Você pode consultar nos cards os indicadores específicos de cada tema...</p>
       </div>
   
       <!-- Cards com efeito 3D -->
       <Card3D
         icon="educacao.svg"
         title="Educação"
-        backText="Veja a quantidade de escolas, educandos, educadores de cada Subprefeitura."
+        backText="Veja a informações sobre as escolas, o orçamento destinado a educação, os professores e os alunos de cada Subprefeitura."
         bgColor="#faca72"
         navLink="/educacao"
       />
       <Card3D
         icon="genero.svg"
         title="Gênero"
-        backText="Informações sobre o lançamento."
+        backText="Veja qual o orçamento destinado pela Prefeitura às mulheres em políticas públicas específicas ou focalizações em políticas públicas mais amplas."
         bgColor="#7163aa"
         navLink="/genero"
       />
       <Card3D
         icon="saude.svg"
         title="Saúde"
-        backText="Detalhes dos componentes utilizados."
+        backText="Veja informações detalhadas sobre o orçamento destinado à saúde, a quantidade de consultas e leitos disponíveis em cada Subprefeitura."
         bgColor="#87c8ef"
         navLink="/saude"
       />
       <Card3D
         icon="urbanismo.svg"
         title="Urbanismo"
-        backText="Dados e gráficos de análise."
+        backText="Veja informações sobre os parques, áreas de risco e políticas públicas de habitação no município de São Paulo."
         bgColor="#a5bb5b"
         navLink="/urbanismo"
       />
       <Card3D
         icon="orcamento.svg"
         title="Regionalização do Orçamento"
-        backText="Informações sobre configuração."
+        backText="Veja como o orçamento da Prefeitura se distribuiu entre as diferentes áreas e como evoluiu ao longo de 2024."
         bgColor="#e57c76"
       />
     </div>
@@ -56,7 +57,7 @@
     grid-template-columns: repeat(3, 1fr);
     grid-auto-rows: 1fr; /* Adicionado para células com altura uniforme */
     gap: 1.5rem;
-    max-width: 62.5rem;
+    max-width: 74.2rem;
     margin: 5vh auto;
     padding: 2rem;
     background-color: transparent;
@@ -65,12 +66,25 @@
   /* Nova célula para texto */
   .info-cell {
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
     border-radius: 0.75rem;
     padding: 1.5rem;
     text-align: center;
     z-index: 12;
+    font-size: 1.5rem;
+  }
+  
+  .info-cell h2 {
+    margin: 0 0 0.5rem 0;
+    font-size: 3.6rem;
+    font-weight: 800;
+    text-transform: uppercase;
+  }
+  
+  .info-cell p {
+    margin: 0;
   }
   
   .text-area-container {
