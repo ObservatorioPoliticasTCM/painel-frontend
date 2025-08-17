2b62297236081fe5d30b61e26c483053cc80681c
Bruno Azuma Balzano <41346451+brunoazuma@users.noreply.github.com>
2025-08-17 02:53:51 -0300

    92 educacao adicionar novos dashboards (#96)
    * appid atualizado
    
    * dashboard de orçamento adicionado
    
    * Ajustados nomes dos dashboards

--- src/views/EducacaoView.vue
+++ src/views/EducacaoView.vue
@@ -1,48 +1,53 @@
 <template>
   <SnapPage>
     <DashboardFrame
-      appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
+      appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
       sheet="dRrpWnj"
       title="Visão geral da educação"
       identity="1"
     />
     <DashboardFrame
-      appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
-      sheet="8a9478a1-23b6-4165-a526-12f9d736aaa0"
-      title="Número de unidades escolares"
-      subtitle="Inclui as unidades escolares particulares"
-      identity="2"
-    />
-    <DashboardFrame
-      appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
-      sheet="nvgxg"
-      title="Perfil dos educadores da rede pública municipal"
+      appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
+      sheet="mSNDU"
+      title="Despesas com educação na rede pública municipal em 2024"
       identity="3"
     />
     <DashboardFrame
-      appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
-      sheet="nNpyj"
-      title="Perfil dos educandos da rede pública municipal"
+      appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
+      sheet="8a9478a1-23b6-4165-a526-12f9d736aaa0"
+      title="Número de escolas da rede pública municipal em 2024"
+      identity="3"
+    />
+    <DashboardFrame
+      appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
+      sheet="nvgxg"
+      title="Perfil dos professores da rede pública municipal em 2024"
       identity="4"
     />
     <DashboardFrame
-      appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
-      sheet="jRpJk"
-      title="Alunos da rede pública municipal beneficiários do Bolsa Família"
+      appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
+      sheet="nNpyj"
+      title="Perfil dos alunos da rede pública municipal em 2024"
       identity="5"
     />
     <DashboardFrame
-      appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
-      sheet="PqxZb"
-      title="Educandos estrangeiros na rede pública municipal"
+      appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
+      sheet="jRpJk"
+      title="Alunos da rede pública municipal beneficiários do Bolsa Família em 2024"
       identity="6"
+    />
+    <DashboardFrame
+      appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
+      sheet="PqxZb"
+      title="Alunos estrangeiros na rede pública municipal em 2024"
+      identity="7"
     />
   </SnapPage>
 </template>
 
 <script setup lang="ts">
-import DashboardFrame from '../components/DashboardFrame.vue'
-import SnapPage from '../components/SnapPage.vue'
+import DashboardFrame from '@/components/DashboardFrame.vue'
+import SnapPage from '@/components/SnapPage.vue'
 </script>
 
 <style scoped></style>
