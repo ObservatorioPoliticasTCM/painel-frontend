c3f3ac35940946d9d90cbaa7d2d217ab2e4721d0
Bruno Azuma Balzano <brunoazuma@usp.br>
2025-08-17 02:42:33 -0300

    appid atualizado

--- src/views/EducacaoView.vue
+++ src/views/EducacaoView.vue
@@ -1,38 +1,38 @@
 <template>
   <SnapPage>
     <DashboardFrame
-      appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
+      appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
       sheet="dRrpWnj"
       title="Visão geral da educação"
       identity="1"
     />
     <DashboardFrame
-      appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
+      appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
       sheet="8a9478a1-23b6-4165-a526-12f9d736aaa0"
       title="Número de unidades escolares"
       subtitle="Inclui as unidades escolares particulares"
       identity="2"
     />
     <DashboardFrame
-      appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
+      appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
       sheet="nvgxg"
       title="Perfil dos educadores da rede pública municipal"
       identity="3"
     />
     <DashboardFrame
-      appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
+      appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
       sheet="nNpyj"
       title="Perfil dos educandos da rede pública municipal"
       identity="4"
     />
     <DashboardFrame
-      appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
+      appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
       sheet="jRpJk"
       title="Alunos da rede pública municipal beneficiários do Bolsa Família"
       identity="5"
     />
     <DashboardFrame
-      appid="21662bb0-1242-4c61-9668-d1ff76e8b530"
+      appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
       sheet="PqxZb"
       title="Educandos estrangeiros na rede pública municipal"
       identity="6"
@@ -41,8 +41,8 @@
 </template>
 
 <script setup lang="ts">
-import DashboardFrame from '../components/DashboardFrame.vue'
-import SnapPage from '../components/SnapPage.vue'
+import DashboardFrame from '@/components/DashboardFrame.vue'
+import SnapPage from '@/components/SnapPage.vue'
 </script>
 
 <style scoped></style>
