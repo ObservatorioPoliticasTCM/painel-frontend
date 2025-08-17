7b8afc39955fdd356b210107063b418efe2b713a
Bruno Azuma Balzano <brunoazuma@usp.br>
2025-08-17 17:49:31 -0300

    adicionado dashboard ne1

--- src/views/GeneroView.vue
+++ src/views/GeneroView.vue
@@ -6,6 +6,12 @@
       title="Despesas em ações voltadas exclusivamente a políticas de promoção aos direitos das mulheres"
       identity="1"
     />
+    <DashboardFrame
+      appid="e808b4ea-ded0-41c6-b5b0-8845934d8369"
+      sheet="b5eaabe4-7475-4e0f-ab78-92f912214961"
+      title="Ações sem foco exclusivo nas mulheres, mas parcialmente focalizadas por iniciativa discricionária da gestão"
+      identity="2"
+    />
   </SnapPage>
 </template>
 
