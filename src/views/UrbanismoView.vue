ae5248ab306a4ab69317453b2a818a47291aec4f
Bruno Azuma Balzano <brunoazuma@usp.br>
2025-08-17 17:18:51 -0300

    Dashboards de habitação adicionados

--- src/views/UrbanismoView.vue
+++ src/views/UrbanismoView.vue
@@ -16,7 +16,31 @@
       appid="22a261d9-7064-4232-956f-232f78547e51"
       sheet="1f71d49e-422e-4314-aa8b-259b8f948658"
       title="Parques novos e requalificados, áreas permeáveis e despesas liquidadas com parques (2024)"
-      identity="2"
+      identity="3"
+    />
+    <DashboardFrame
+      appid="5894c40c-193a-4bfc-8b05-0cec25c9a09c"
+      sheet="da5e8886-a388-4a4d-9835-8990c3744f2a"
+      title="Déficit habitacional, domicílios não ocupados e domicílios em favelas"
+      identity="4"
+    />
+    <DashboardFrame
+      appid="58f8b63f-5302-4e4b-9260-dc74b69c57f9"
+      sheet="6fe6d2e2-43de-4c67-8aba-6bd99ad3ba74"
+      title="Famílias beneficiárias do programa de Auxílio Aluguel (2024)"
+      identity="5"
+    />
+    <DashboardFrame
+      appid="2be0f077-d541-4b52-a2bf-7b05c1b72400"
+      sheet="NAtPM"
+      title="Famílias beneficiadas por regularização fundiária e obras em assentamentos precários (2024)"
+      identity="6"
+    />
+    <DashboardFrame
+      appid="18956ab0-d917-475f-8c94-aba437c955fc"
+      sheet="SHsCbm"
+      title="Despesas liquidadas na função Habitação em 2024"
+      identity="7"
     />
   </SnapPage>
 </template>
