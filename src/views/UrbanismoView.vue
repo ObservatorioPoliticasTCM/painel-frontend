1b6fdd84e809d27eac1c376dce10073f5f8ec4a2
Bruno Azuma Balzano <41346451+brunoazuma@users.noreply.github.com>
2025-08-17 17:36:41 -0300

    91 urbanismo adicionar novos dashboards (#99)
    * Atualizados dashboards existentes
    
    * Adicionado dashboard de novos parques
    
    * Dashboards de habitação adicionados
    
    * Adicionado placeholder para dashboard de produção habitacional

--- src/views/UrbanismoView.vue
+++ src/views/UrbanismoView.vue
@@ -1,22 +1,52 @@
 <template>
   <SnapPage>
     <DashboardFrame
-      appid="3fc394bc-d567-4b1b-94f4-3f87ea76a450"
-      sheet="b779c56d-dee5-4a71-a575-d7e42afa8789"
-      title="População em áreas de risco"
+      appid="e03b5a17-290f-49a7-8202-e9a05b446f35"
+      sheet="e9731105-32d5-45a6-a2ac-972df82b3d6b"
+      title="População em risco geológico e hidrológico (2025)"
       identity="1"
     />
     <DashboardFrame
-      appid="73f920a4-af97-412a-82d0-a12c13dd9ec0"
+      appid="b4ec6f8c-40e8-473f-b130-359e44df750e"
       sheet="QMPruE"
-      title="Área de parques municipais"
+      title="Área de parques municipais e estaduais (2024)"
       identity="2"
     />
     <DashboardFrame
-      appid="fee34b2e-4e55-4960-8358-c2a9178642ff"
-      sheet="bjqsPD"
-      title="Quantidade de pessoas atendidas no programa Atende+"
+      appid="22a261d9-7064-4232-956f-232f78547e51"
+      sheet="1f71d49e-422e-4314-aa8b-259b8f948658"
+      title="Parques novos e requalificados, áreas permeáveis e despesas liquidadas com parques (2024)"
       identity="3"
+    />
+    <DashboardFrame
+      appid="5894c40c-193a-4bfc-8b05-0cec25c9a09c"
+      sheet="da5e8886-a388-4a4d-9835-8990c3744f2a"
+      title="Déficit habitacional, domicílios não ocupados e domicílios em favelas"
+      identity="4"
+    />
+    <DashboardFrame
+      appid="58f8b63f-5302-4e4b-9260-dc74b69c57f9"
+      sheet="6fe6d2e2-43de-4c67-8aba-6bd99ad3ba74"
+      title="Famílias beneficiárias do programa de Auxílio Aluguel (2024)"
+      identity="5"
+    />
+    <DashboardFrame
+      appid="5894c40c-193a-4bfc-8b05-0cec25c9a09c"
+      sheet="da5e8886-a388-4a4d-9835-8990c3744f2a"
+      title="Unidades habitacionais entregues ou contratadas (2024)"
+      identity="6"
+    />
+    <DashboardFrame
+      appid="2be0f077-d541-4b52-a2bf-7b05c1b72400"
+      sheet="NAtPM"
+      title="Famílias beneficiadas por regularização fundiária e obras em assentamentos precários (2024)"
+      identity="7"
+    />
+    <DashboardFrame
+      appid="18956ab0-d917-475f-8c94-aba437c955fc"
+      sheet="SHsCbm"
+      title="Despesas liquidadas na função Habitação em 2024"
+      identity="8"
     />
   </SnapPage>
 </template>
