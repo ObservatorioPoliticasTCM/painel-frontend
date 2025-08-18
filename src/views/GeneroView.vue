d8976ddd11632b2ea94ba293902ad5574fd8ea72
Bruno Azuma Balzano <brunoazuma@usp.br>
2025-08-17 22:22:00 -0300

    Adicionados dashboards NE2

--- src/views/GeneroView.vue
+++ src/views/GeneroView.vue
@@ -12,6 +12,41 @@
       title="Ações sem foco exclusivo nas mulheres, mas parcialmente focalizadas por iniciativa discricionária da gestão (2023)"
       identity="2"
     />
+    <DashboardFrame
+      appid="e808b4ea-ded0-41c6-b5b0-8845934d8369"
+      sheet="3d5ba550-f947-4ba8-a12e-31ee20c05521"
+      title="Plano Municipal de Políticas para as Mulheres - Habitação"
+      subtitle="Ações sem foco exclusivo nas mulheres, mas que cumprem compromissos do Plano Municipal de Políticas para as Mulheres na categoria de habitação"
+      identity="3"
+    />
+    <DashboardFrame
+      appid="e808b4ea-ded0-41c6-b5b0-8845934d8369"
+      sheet="62d5961d-3b5a-4872-adb5-0c79b489f70c"
+      title="Plano Municipal de Políticas para as Mulheres - Educação"
+      subtitle="Ações sem foco exclusivo nas mulheres, mas que cumprem compromissos do Plano Municipal de Políticas para as Mulheres na categoria de educação"
+      identity="4"
+    />
+    <DashboardFrame
+      appid="e808b4ea-ded0-41c6-b5b0-8845934d8369"
+      sheet="fde5eb98-c713-45ad-b134-fc6d58a188e5"
+      title="Plano Municipal de Políticas para as Mulheres - Assistencia Social"
+      subtitle="Ações sem foco exclusivo nas mulheres, mas que cumprem compromissos do Plano Municipal de Políticas para as Mulheres na categoria de assistência social"
+      identity="5"
+    />
+    <DashboardFrame
+      appid="e808b4ea-ded0-41c6-b5b0-8845934d8369"
+      sheet="7931a494-f4b5-4f34-a3e2-c57f08b8d53a"
+      title="Plano Municipal de Políticas para as Mulheres - Direitos Humanos"
+      subtitle="Ações sem foco exclusivo nas mulheres, mas que cumprem compromissos do Plano Municipal de Políticas para as Mulheres na categoria de direitos humanos"
+      identity="6"
+    />
+    <DashboardFrame
+      appid="e808b4ea-ded0-41c6-b5b0-8845934d8369"
+      sheet="49e484ce-2ad7-41a1-b727-0680f3a9036b"
+      title="Plano Municipal de Políticas para as Mulheres - Trabalho"
+      subtitle="Ações sem foco exclusivo nas mulheres, mas que cumprem compromissos do Plano Municipal de Políticas para as Mulheres na categoria de trabalho"
+      identity="7"
+    />
   </SnapPage>
 </template>
 
