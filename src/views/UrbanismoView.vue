032915ed141a105c4a5905c5b773b8fdb8fab5d8
Bruno Azuma Balzano <brunoazuma@usp.br>
2025-08-18 18:12:55 -0300

    Adicionados novos aplicativos

--- src/views/UrbanismoView.vue
+++ src/views/UrbanismoView.vue
@@ -19,34 +19,46 @@
       identity="3"
     />
     <DashboardFrame
+      appid="14f47000-5a08-4b2a-b198-d7ae6b6d55b2"
+      sheet="5de719a0-3b7d-4000-a4ec-64c9e35d9ad4"
+      title="Despesas liquidadas em Gestão dos Riscos e Promoção da Resiliência a Desastres e Eventos Críticos (2024)"
+      identity="4"
+    />
+    <DashboardFrame
       appid="5894c40c-193a-4bfc-8b05-0cec25c9a09c"
       sheet="da5e8886-a388-4a4d-9835-8990c3744f2a"
       title="Déficit habitacional, domicílios não ocupados e domicílios em favelas"
-      identity="4"
+      identity="5"
     />
     <DashboardFrame
       appid="58f8b63f-5302-4e4b-9260-dc74b69c57f9"
       sheet="6fe6d2e2-43de-4c67-8aba-6bd99ad3ba74"
       title="Famílias beneficiárias do programa de Auxílio Aluguel (2024)"
-      identity="5"
+      identity="6"
     />
     <DashboardFrame
       appid="97daaa3e-d91e-4eaf-aa6b-1cb4772239bb"
       sheet="hJfCYXg"
       title="Unidades habitacionais entregues ou contratadas (2024)"
-      identity="6"
+      identity="7"
     />
     <DashboardFrame
       appid="2be0f077-d541-4b52-a2bf-7b05c1b72400"
       sheet="NAtPM"
       title="Famílias beneficiadas por regularização fundiária e obras em assentamentos precários (2024)"
-      identity="7"
+      identity="8"
+    />
+    <DashboardFrame
+      appid="f571d56a-108d-4a83-b865-45255c72b23d"
+      sheet="avsWR"
+      title="Realização Física Financeira em políticas de Acesso à Moradia Adequada de 2022 a 2025"
+      identity="9"
     />
     <DashboardFrame
       appid="18956ab0-d917-475f-8c94-aba437c955fc"
       sheet="SHsCbm"
-      title="Despesas liquidadas na função Habitação em 2024"
-      identity="8"
+      title="Despesas liquidadas em toda a função Habitação em 2024"
+      identity="10"
     />
   </SnapPage>
 </template>
