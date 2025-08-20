22ca4ab87edc4a8de470ce607e364a036f5d714d
Bruno Azuma Balzano <brunoazuma@usp.br>
2025-08-20 11:53:43 -0300

    Propriedades identity ajustadas

--- src/views/UrbanismoView.vue
+++ src/views/UrbanismoView.vue
@@ -10,19 +10,19 @@
       appid="14f47000-5a08-4b2a-b198-d7ae6b6d55b2"
       sheet="5de719a0-3b7d-4000-a4ec-64c9e35d9ad4"
       title="Despesas em Gestão dos Riscos e Promoção da Resiliência a Desastres e Eventos Críticos (2024)"
-      identity="4"
+      identity="2"
     />
     <DashboardFrame
       appid="b4ec6f8c-40e8-473f-b130-359e44df750e"
       sheet="QMPruE"
       title="Área de parques municipais e estaduais (2024)"
-      identity="2"
+      identity="3"
     />
     <DashboardFrame
       appid="22a261d9-7064-4232-956f-232f78547e51"
       sheet="1f71d49e-422e-4314-aa8b-259b8f948658"
       title="Parques novos e requalificados, áreas permeáveis e despesas liquidadas com parques (2024)"
-      identity="3"
+      identity="4"
     />
     <DashboardFrame
       appid="5894c40c-193a-4bfc-8b05-0cec25c9a09c"
@@ -34,19 +34,19 @@
       appid="97daaa3e-d91e-4eaf-aa6b-1cb4772239bb"
       sheet="hJfCYXg"
       title="Unidades habitacionais entregues ou contratadas (2024)"
-      identity="7"
+      identity="6"
     />
     <DashboardFrame
       appid="2be0f077-d541-4b52-a2bf-7b05c1b72400"
       sheet="NAtPM"
       title="Famílias beneficiadas por regularização fundiária e obras em assentamentos precários (2024)"
-      identity="8"
+      identity="7"
     />
     <DashboardFrame
       appid="58f8b63f-5302-4e4b-9260-dc74b69c57f9"
       sheet="6fe6d2e2-43de-4c67-8aba-6bd99ad3ba74"
       title="Famílias beneficiárias do programa de Auxílio Aluguel (2024)"
-      identity="6"
+      identity="8"
     />
     <DashboardFrame
       appid="f571d56a-108d-4a83-b865-45255c72b23d"
@@ -58,13 +58,13 @@
       appid="f571d56a-108d-4a83-b865-45255c72b23d"
       sheet="5548d5e2-5953-4beb-aaea-1345aae8f5d1"
       title="Realização Financeira em políticas de Acesso à Moradia Adequada de 2022 a 2025"
-      identity="9"
+      identity="10"
     />
     <DashboardFrame
       appid="18956ab0-d917-475f-8c94-aba437c955fc"
       sheet="SHsCbm"
       title="Despesas liquidadas em toda a função Habitação em 2024"
-      identity="10"
+      identity="11"
     />
   </SnapPage>
 </template>
