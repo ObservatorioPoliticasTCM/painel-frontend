8105f02aa0e7da94566f81d9ef28c6cdbfba4d6a
Bruno Azuma Balzano <41346451+brunoazuma@users.noreply.github.com>
2025-08-20 11:42:06 -0300

    36 remover dashboard inicial (#125)
    * Dashboard da home removido

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,9 +4,6 @@
     <SnapContainer>
       <FrontCover />
       <CardGrid />
-      <DashboardFrame appid="13e05213-9609-45cd-86df-5cbf75836d79"
-                      sheet="6afd5fd9-e62b-45d8-a4e9-808bf98d534d"
-                      id="dashboard-home" />
       <Sitemap />
       <Footer />
     </SnapContainer>
@@ -16,7 +13,6 @@
 <script setup lang="ts">
 import FrontCover from '../components/FrontCover.vue'
 import CardGrid from '../components/CardGrid.vue'
-import DashboardFrame from '../components/DashboardFrame.vue'
 import SnapContainer from '../components/SnapContainer.vue'
 import Background from '../components/Background.vue'
 import Footer from '../components/Footer.vue'
@@ -28,10 +24,5 @@ import Sitemap from '../components/Sitemap.vue'
   min-height: 100vh;
   overflow: hidden;
   background-color: #121212; /* fundo escuro */
-}
-
-#dashboard-home {
-  position: relative;
-  z-index: 30;
 }
 </style>
