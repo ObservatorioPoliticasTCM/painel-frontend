dcbd828afd5bed2d1446f00d6fac672bbf985ba6
Bruno Azuma Balzano <brunoazuma@usp.br>
2025-10-03 16:16:15 -0300

    Adicionado componente de especialistas

---
+++ src/components/Specialists.vue
@@ -0,0 +1,23 @@
+<template>
+  <NameGroup name="Especialistas">
+    <NameAndTitle name="Nelson Marconi" title="Coordenador do curso de graduação em administração pública EAESP FGV." />
+    <NameAndTitle name="Denise Carreira" title="Professora da Faculdade de Educação da Universidade de São Paulo." />
+    <NameAndTitle name="Maria Amelia Almeida Teles" title="Coordenadora da União de Mulheres de São Paulo e do Programa de Promotoras Legais Populares." />
+    <NameAndTitle name="Claudio Luiz Camargo" title="Professor associado da Universidade Federal do ABC e pesquisador do NEAMP da PUCSP e do LabLivre da UFABC." />
+    <NameAndTitle name="Maria Carlotto" title="Coordenadora do Programa de Pós-Graduação em Economia Política Mundial da Universidade Federal do ABC." />
+    <NameAndTitle name="Roberta Peres" title="Docente da Universidade Federal do ABC. Coordenadora do Bacharelado em Ciências e Humanidades." />
+    <NameAndTitle name="Rute Alonso da Silva" title="Presidenta da União de Mulheres do Município de São Paulo e coordenadora do projeto Promotoras Legais Populares." />
+    <NameAndTitle name="Danielle Bivanco" title="Professora, supervisora do Internato em Medicina de Família e Comunidade e participante do Núcleo de Apoio Pedagógico da Universidade São Caetano do Sul." />
+    <NameAndTitle name="Ronaldo Malheiros Figueira" title="Professor do curso de pós-graduação do Grupo Ecossistema ÂNIMA e professor do Centro Universitário Sant'Anna." />
+    <NameAndTitle name="Valter Caldana" title="Professor da Faculdade de Arquitetura e Urbanismo da Universidade Presbiteriana Mackenzie. É pesquisador do Laboratório Citer da Universidade de Ferrara-Itália e coordenador do LPP - Laboratório de Projetos e Políticas Públicas." />
+    <NameAndTitle name="Marilia Roggero" title="Doutora em Ciências pelo Programa de Pós-Graduação em Geografia Física da Universidade de São Paulo. É geógrafa na Diagonal Social." />
+    <NameAndTitle name="Daniela Perre" title="Graduada em Arquitetura e Urbanismo pela Universidade de São Paulo e pós-graduada em Direito Público Municipal pela Escola Superior de Gestão e Contas Públicas." />
+    <NameAndTitle name="Tomás Wissenbach" title="Geógrafo e mestre em Geografia Humana pela USP e doutor em Administração Pública e Governo pela FGV-SP. É pesquisador e coordenador de projetos no Cebrap e professor do Cebrap.Lab." />
+    <NameAndTitle name="Renato Eliseu Costa" title="Professor da ESALQ-USP, onde integra o Departamento de Administração e Economia e o programa de pós-graduação stricto sensu em Administração. Também atua como docente do mestrado e doutorado em Políticas Públicas da UFABC." />
+  </NameGroup>
+</template>
+
+<script setup lang="ts">
+import NameGroup from '@/components/NameGroup.vue'
+import NameAndTitle from '@/components/NameAndTitle.vue'
+</script>
