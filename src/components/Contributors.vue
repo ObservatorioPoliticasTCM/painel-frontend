62f617a976bb8d139aa36f8fe17e1998bbc68304
Bruno Azuma Balzano <brunoazuma@usp.br>
2025-10-03 16:19:51 -0300

    Adicionado componente de colaboradores

---
+++ src/components/Contributors.vue
@@ -0,0 +1,17 @@
+<template>
+  <NameGroup name="Colaboradores">
+    <NameAndTitle name="Fulvio Giannella Junior" title="Assessor de Imprensa" showBottomBorder />
+    <NameAndTitle name="Dilson Ferreira da Cruz Junior" title="Auditor de Controle Externo" showBottomBorder />
+    <NameAndTitle name="Rafael Valverde Arantes" title="Secretário de Controle Externo" showBottomBorder />
+    <NameAndTitle name="Maria Clara Watanabe Tanabe" title="Auditora de Controle Externo" showBottomBorder />
+    <NameAndTitle name="Fredy Henrique Miller" title="Coordenador de Controle Externo II" showBottomBorder />
+    <NameAndTitle name="Luiz Gustavo de Oliveira Vieira" title="Coordenador de Controle Externo IV" showBottomBorder />
+    <NameAndTitle name="Rafael Rocha Lins" title="Coordenador de Controle Externo VI" showBottomBorder />
+    <NameAndTitle name="Dimitri Fabricio Carvalho Rodermel" title="Coordenador de Controle Externo VII" />
+  </NameGroup>
+</template>
+
+<script setup lang="ts">
+import NameGroup from '@/components/NameGroup.vue'
+import NameAndTitle from '@/components/NameAndTitle.vue'
+</script>
