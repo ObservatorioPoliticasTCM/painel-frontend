55fb6dd0f1ef7d3844d6313303f4ac564e8b14ea
Bruno Azuma Balzano <brunoazuma@usp.br>
2025-10-03 15:59:30 -0300

    Adicionado componente de nome e título

---
+++ src/components/NameAndTitle.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="name-and-title" :class="{ 'with-border': showBottomBorder }">
+    <div class="name">{{ name }}</div>
+    <div class="title">{{ title }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { name, title, showBottomBorder = false } = defineProps<{ name: string; title: string; showBottomBorder?: boolean }>()
+
+</script>
+
+<style scoped>
+.name-and-title {
+  padding: 0em 0;
+  text-align: left;
+}
+.name-and-title.with-border {
+  border-bottom: 0.0625em solid #fff;
+  margin-bottom: 0.4em;
+}
+.name {
+  font-weight: bold;
+  font-size: 1.05em;
+  margin-bottom: 0.35em;
+}
+.title {
+  font-size: 0.95em;
+  line-height: 1.35em;
+}
+</style>
