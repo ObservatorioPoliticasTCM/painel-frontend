eb969dceb8ffa26a970b6cda9a3c656dfaf834d3
Saboleu <alvesxdxd28@gmail.com>
2025-10-09 14:42:59 -0300

    Revisão de titulo e adição das datas de referencia de todas as dashboards com exceção de gênero pois não foi necessário.

--- src/views/EducacaoView.vue
+++ src/views/EducacaoView.vue
@@ -3,43 +3,43 @@
     <DashboardFrame
       appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
       sheet="dRrpWnj"
-      title="Visão geral da educação"
+      title="Visão geral da educação (2024)"
       identity="1"
     />
     <DashboardFrame
       appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
       sheet="mSNDU"
-      title="Despesas com educação na rede pública municipal em 2024"
+      title="Despesas com educação na rede pública municipal (2024)"
       identity="3"
     />
     <DashboardFrame
       appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
       sheet="8a9478a1-23b6-4165-a526-12f9d736aaa0"
-      title="Número de escolas da rede pública municipal em 2024"
+      title="Número de escolas da rede pública municipal (2024)"
       identity="3"
     />
     <DashboardFrame
       appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
       sheet="nvgxg"
-      title="Perfil dos professores da rede pública municipal em 2024"
+      title="Perfil dos professores da rede pública municipal (2024)"
       identity="4"
     />
     <DashboardFrame
       appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
       sheet="nNpyj"
-      title="Perfil dos alunos da rede pública municipal em 2024"
+      title="Perfil dos alunos da rede pública municipal (2024)"
       identity="5"
     />
     <DashboardFrame
       appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
       sheet="jRpJk"
-      title="Alunos da rede pública municipal beneficiários do Bolsa Família em 2024"
+      title="Alunos da rede pública municipal beneficiários do Bolsa Família (2024)"
       identity="6"
     />
     <DashboardFrame
       appid="ee3bc48f-da4d-403a-a4fe-4da01cb29ee9"
       sheet="PqxZb"
-      title="Alunos estrangeiros na rede pública municipal em 2024"
+      title="Alunos estrangeiros na rede pública municipal (2024)"
       identity="7"
     />
   </SnapPage>
--- src/views/UrbanismoView.vue
+++ src/views/UrbanismoView.vue
@@ -3,25 +3,25 @@
     <DashboardFrame
       appid="ae6af572-20d7-4971-87ca-c472c0bde58a"
       sheet="e9731105-32d5-45a6-a2ac-972df82b3d6b"
-      title="População em risco geológico e \n hidrológico (2025)"
+      title="População em risco geológico e hidrológico"
       identity="1"
     />
     <DashboardFrame
       appid="14f47000-5a08-4b2a-b198-d7ae6b6d55b2"
       sheet="5de719a0-3b7d-4000-a4ec-64c9e35d9ad4"
-      title="Despesas em Gestão dos Riscos e Promoção da Resiliência a Desastres e Eventos Críticos (2024)"
+      title="Despesas em Gestão dos Riscos e Promoção da Resiliência a Desastres e Eventos Críticos"
       identity="2"
     />
     <DashboardFrame
       appid="b4ec6f8c-40e8-473f-b130-359e44df750e"
       sheet="QMPruE"
-      title="Área de parques municipais e estaduais (2024)"
+      title="Área de parques municipais e estaduais"
       identity="3"
     />
     <DashboardFrame
       appid="22a261d9-7064-4232-956f-232f78547e51"
       sheet="1f71d49e-422e-4314-aa8b-259b8f948658"
-      title="Parques novos e requalificados, áreas permeáveis e despesas liquidadas com parques (2024)"
+      title="Parques novos e requalificados, áreas permeáveis e despesas liquidadas com parques"
       identity="4"
     />
     <DashboardFrame
@@ -33,7 +33,7 @@
     <DashboardFrame
       appid="97daaa3e-d91e-4eaf-aa6b-1cb4772239bb"
       sheet="hJfCYXg"
-      title="Unidades habitacionais entregues ou contratadas (2024)"
+      title="Unidades habitacionais entregues ou contratadas"
       identity="6"
     />
     <DashboardFrame
@@ -45,25 +45,25 @@
     <DashboardFrame
       appid="58f8b63f-5302-4e4b-9260-dc74b69c57f9"
       sheet="6fe6d2e2-43de-4c67-8aba-6bd99ad3ba74"
-      title="Famílias beneficiárias do programa de Auxílio Aluguel (2024)"
+      title="Famílias beneficiárias do programa de Auxílio Aluguel"
       identity="8"
     />
     <DashboardFrame
       appid="f571d56a-108d-4a83-b865-45255c72b23d"
       sheet="avsWR"
-      title="Realização Física em políticas de Acesso à Moradia Adequada de 2022 a 2025"
+      title="Realização Física em políticas de Acesso à Moradia Adequada"
       identity="9"
     />
     <DashboardFrame
       appid="f571d56a-108d-4a83-b865-45255c72b23d"
       sheet="5548d5e2-5953-4beb-aaea-1345aae8f5d1"
-      title="Realização Financeira em políticas de Acesso à Moradia Adequada de 2022 a 2025"
+      title="Realização Financeira em políticas de Acesso à Moradia Adequada"
       identity="10"
     />
     <DashboardFrame
       appid="18956ab0-d917-475f-8c94-aba437c955fc"
       sheet="SHsCbm"
-      title="Despesas liquidadas em toda a função Habitação em 2024"
+      title="Despesas liquidadas em toda a função Habitação"
       identity="11"
     />
   </SnapPage>
