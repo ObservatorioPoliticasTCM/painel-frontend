7bd103079e55bf8d2dfd939352cfe0b954d8bcae
Samuel Bento Alves <alvesxdxd28@gmail.com>
2025-10-23 14:13:22 -0300

    Reorganização das dashboards e separação de leitos e óbitos.

--- src/views/SaudeView.vue
+++ src/views/SaudeView.vue
@@ -2,29 +2,30 @@
   <SnapPage>
     <DashboardFrame
       appid="585f0398-5876-4e29-98db-a53e3e013dc4"
-      sheet="KRXJH"
-      title="Visão geral da saúde"
+      sheet="EnGpsY"
+      title="Número de consultas em 2023"
+      subtitle="Inclui as unidades municipais, estaduais e federais"
       identity="1"
+    />
+    <DashboardFrame
+    appid="585f0398-5876-4e29-98db-a53e3e013dc4"
+    sheet="kPfKtR"
+    title="Leitos"
+    subtitle="Inclui leitos das unidades municipais, estaduais e federais"
+    identity="2"
+    />
+    <DashboardFrame
+    appid="585f0398-5876-4e29-98db-a53e3e013dc4"
+    sheet="hSjCf"
+    title="Óbitos"
+    subtitle="Inclui leitos das unidades municipais, estaduais e federais"
+    identity="3"
     />
     <DashboardFrame
       appid="585f0398-5876-4e29-98db-a53e3e013dc4"
       sheet="XFZvFPN"
       title="Despesas em atenção Básica e especializada"
       subtitle="Inclui apenas despesas municipais"
-      identity="2"
-    />
-    <DashboardFrame
-      appid="585f0398-5876-4e29-98db-a53e3e013dc4"
-      sheet="EnGpsY"
-      title="Número de consultas em 2023"
-      subtitle="Inclui as unidades municipais, estaduais e federais"
-      identity="3"
-    />
-    <DashboardFrame
-      appid="585f0398-5876-4e29-98db-a53e3e013dc4"
-      sheet="kPfKtR"
-      title="Leitos e mortalidade"
-      subtitle="Inclui leitos das unidades municipais, estaduais e federais"
       identity="4"
     />
   </SnapPage>
