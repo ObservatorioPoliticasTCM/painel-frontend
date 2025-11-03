b80ef6aaa363db1256a3a7c604569be00cbebfe7
Samuel Bento Alves <160528743+Saboleu@users.noreply.github.com>
2025-11-03 16:08:59 -0300

    Revisão de titulo e adição das datas de referencia de todas as dashboards (#160)
    * Revisão de titulo e adição das datas de referencia de todas as dashboards com exceção de gênero pois não foi necessário.
    
    * Atualiza títulos dos dashboards com anos de referência para melhor clareza
    
    * Ajustados anos dos últimos títulos
    
    ---------
    
    Co-authored-by: Bruno Azuma Balzano <brunoazuma@usp.br>

--- src/views/OrcamentoView.vue
+++ src/views/OrcamentoView.vue
@@ -4,19 +4,19 @@
       appid="01ec34f7-4862-43f5-bd71-0413d71dbe97"
       sheet="UMJqywF"
       identity="1"
-      title="Variação do Orçamento por Função \nde Governo em 2024"
+      title="Variação do Orçamento por Função de Governo (2024)"
     />
     <DashboardFrame
       appid="6c0cd072-faf6-4bc4-9251-7ef5479ff6af"
       sheet="bd2c8bf9-d161-43ed-a967-b96342558630"
       identity="2"
-      title="Variação detalhada do Orçamento por Função \nde Governo em 2024"
+      title="Variação detalhada do Orçamento por Função de Governo (2024)"
     />
     <DashboardFrame
       appid="daf572a6-2fda-44c6-b100-77dd018de06f"
       sheet="826cc7f9-9f5d-498f-aba1-af1bd4aca5ba"
       identity="3"
-      title="Variação detalhada do Orçamento por Órgão \nem 2024"
+      title="Variação detalhada do Orçamento por Órgão (2024)"
     />
   </SnapPage>
 </template>
--- src/views/SaudeView.vue
+++ src/views/SaudeView.vue
@@ -3,28 +3,28 @@
     <DashboardFrame
       appid="585f0398-5876-4e29-98db-a53e3e013dc4"
       sheet="EnGpsY"
-      title="Número de consultas em 2023"
+      title="Número de consultas (2024)"
       subtitle="Inclui as unidades municipais, estaduais e federais"
       identity="1"
     />
     <DashboardFrame
     appid="585f0398-5876-4e29-98db-a53e3e013dc4"
     sheet="kPfKtR"
-    title="Leitos"
+    title="Leitos (2024)"
     subtitle="Inclui leitos das unidades municipais, estaduais e federais"
     identity="2"
     />
     <DashboardFrame
     appid="585f0398-5876-4e29-98db-a53e3e013dc4"
     sheet="hSjCf"
-    title="Óbitos"
+    title="Óbitos (2021)"
     subtitle="Inclui óbitos de residentes do município de São Paulo"
     identity="3"
     />
     <DashboardFrame
       appid="585f0398-5876-4e29-98db-a53e3e013dc4"
       sheet="XFZvFPN"
-      title="Despesas em atenção Básica e especializada"
+      title="Despesas em atenção Básica e especializada (2024)"
       subtitle="Inclui apenas despesas municipais"
       identity="4"
     />
