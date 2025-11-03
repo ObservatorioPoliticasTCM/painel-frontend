0fd1c96ed70de81ac3f004e05512a2c18063587f
Saboleu <alvesxdxd28@gmail.com>
2025-11-03 15:23:44 -0300

    Atualiza títulos dos dashboards com anos de referência para melhor clareza

--- src/views/UrbanismoView.vue
+++ src/views/UrbanismoView.vue
@@ -3,25 +3,25 @@
     <DashboardFrame
       appid="ae6af572-20d7-4971-87ca-c472c0bde58a"
       sheet="e9731105-32d5-45a6-a2ac-972df82b3d6b"
-      title="População em risco geológico e hidrológico"
+      title="População em risco geológico e hidrológico (ago/2025)"
       identity="1"
     />
     <DashboardFrame
       appid="14f47000-5a08-4b2a-b198-d7ae6b6d55b2"
       sheet="5de719a0-3b7d-4000-a4ec-64c9e35d9ad4"
-      title="Despesas em Gestão dos Riscos e Promoção da Resiliência a Desastres e Eventos Críticos"
+      title="Despesas em Gestão dos Riscos e Promoção da Resiliência a Desastres e Eventos Críticos (2024)"
       identity="2"
     />
     <DashboardFrame
       appid="b4ec6f8c-40e8-473f-b130-359e44df750e"
       sheet="QMPruE"
-      title="Área de parques municipais e estaduais"
+      title="Área de parques municipais e estaduais (2024)"
       identity="3"
     />
     <DashboardFrame
       appid="22a261d9-7064-4232-956f-232f78547e51"
       sheet="1f71d49e-422e-4314-aa8b-259b8f948658"
-      title="Parques novos e requalificados, áreas permeáveis e despesas liquidadas com parques"
+      title="Parques novos e requalificados, áreas permeáveis e despesas liquidadas com parques (2024)"
       identity="4"
     />
     <DashboardFrame
@@ -39,7 +39,7 @@
     <DashboardFrame
       appid="2be0f077-d541-4b52-a2bf-7b05c1b72400"
       sheet="NAtPM"
-      title="Famílias beneficiadas por regularização fundiária e obras em assentamentos precários (2024)"
+      title="Famílias beneficiadas por regularização fundiária e obras em assentamentos precários (dez/2024)"
       identity="7"
     />
     <DashboardFrame
@@ -63,7 +63,7 @@
     <DashboardFrame
       appid="18956ab0-d917-475f-8c94-aba437c955fc"
       sheet="SHsCbm"
-      title="Despesas liquidadas em toda a função Habitação"
+      title="Despesas liquidadas em toda a função Habitação (2024)"
       identity="11"
     />
   </SnapPage>
