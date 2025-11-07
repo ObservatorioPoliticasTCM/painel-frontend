bc739985054ef6f5cc9330bdc3cf6f4c755e0c95
Samuel Bento Alves <160528743+Saboleu@users.noreply.github.com>
2025-11-07 15:49:26 -0300

    View revertida para a versão da main

--- src/views/SaudeView.vue
+++ src/views/SaudeView.vue
@@ -1,11 +1,19 @@
 <template>
   <SnapPage>
-    <IntroPanel
-      title="Saúde - Introdução"
-      text=""
-      :icon="saudeIcon"
-    >
-    </IntroPanel>
+    <DashboardFrame
+      appid="585f0398-5876-4e29-98db-a53e3e013dc4"
+      sheet="EnGpsY"
+      title="Número de consultas (2024)"
+      subtitle="Inclui as unidades municipais, estaduais e federais"
+      identity="1"
+    />
+    <DashboardFrame
+    appid="585f0398-5876-4e29-98db-a53e3e013dc4"
+    sheet="kPfKtR"
+    title="Leitos (2024)"
+    subtitle="Inclui leitos das unidades municipais, estaduais e federais"
+    identity="2"
+    />
     <DashboardFrame
     appid="585f0398-5876-4e29-98db-a53e3e013dc4"
     sheet="hSjCf"
@@ -20,21 +28,12 @@
       subtitle="Inclui apenas despesas municipais"
       identity="4"
     />
-    <DashboardFrame
-      appid="585f0398-5876-4e29-98db-a53e3e013dc4"
-      sheet="XFZvFPN"
-      title="Despesas em atencao basica e especializada"
-      subtitle="Inclui apenas despesas municipais"
-      identity="2"
-    />
   </SnapPage>
 </template>
 
 <script setup lang="ts">
 import DashboardFrame from '../components/DashboardFrame.vue'
-import IntroPanel from '../components/IntroPanel.vue'
 import SnapPage from '../components/SnapPage.vue'
-import saudeIcon from '@/assets/saude-black.svg'
 </script>
 
 <style scoped></style>
