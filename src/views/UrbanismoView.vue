8f36337801fc8f44efedc71eee5bc1bbcd83b3a7
Bruno Azuma Balzano <brunoazuma@usp.br>
2025-11-10 13:12:53 -0300

    Adicionados links aos dashboards do gt urbanismo

--- src/views/UrbanismoView.vue
+++ src/views/UrbanismoView.vue
@@ -6,6 +6,7 @@
       title="População em risco geológico e hidrológico (ago/2025)"
       identity="1"
       methodology-link="/infos/notas/Urbanismo/População em risco geológico e hidrológico (2025).pdf"
+      download-link="/dados/urbanismo/populacao-em-risco-geologico-e-hidrologico.zip"
     />
     <DashboardFrame
       appid="14f47000-5a08-4b2a-b198-d7ae6b6d55b2"
@@ -18,6 +19,7 @@
       sheet="QMPruE"
       title="Área de parques municipais e estaduais (2024)"
       identity="3"
+      download-link="/dados/urbanismo/area-de-parques-municipais-e-estaduais.zip"
     />
     <DashboardFrame
       appid="22a261d9-7064-4232-956f-232f78547e51"
@@ -25,42 +27,49 @@
       title="Parques novos e requalificados, áreas permeáveis e despesas liquidadas com parques (2024)"
       identity="4"
       methodology-link="/infos/notas/Urbanismo/Parques novos e requalificados, áreas permeáveis e despesas liquidadas com parques (2024).pdf"
+      download-link="/dados/urbanismo/parques-novos-e-requalificados-area-permeavel-e-despesas-liquidadas-com-parques.zip"
     />
     <DashboardFrame
       appid="5894c40c-193a-4bfc-8b05-0cec25c9a09c"
       sheet="da5e8886-a388-4a4d-9835-8990c3744f2a"
       title="Déficit habitacional, domicílios não ocupados e domicílios em favelas"
       identity="5"
+      download-link="/dados/urbanismo/deficit-habitacional-domicilios-nao-ocupados-e-domicilios-em-favelas.zip"
     />
     <DashboardFrame
       appid="97daaa3e-d91e-4eaf-aa6b-1cb4772239bb"
       sheet="hJfCYXg"
       title="Unidades habitacionais entregues ou contratadas (2024)"
       identity="6"
+      download-link="/dados/urbanismo/unidades-habitacionais-entregues-ou-contratadas.zip"
     />
     <DashboardFrame
       appid="2be0f077-d541-4b52-a2bf-7b05c1b72400"
       sheet="NAtPM"
       title="Famílias beneficiadas por regularização fundiária e obras em assentamentos precários (2024)"
       identity="7"
+      download-link="/dados/urbanismo/familias-beneficiadas-por-regularizacao-fundiaria-e-obras-em-assentamentos-precarios.zip"
     />
     <DashboardFrame
       appid="58f8b63f-5302-4e4b-9260-dc74b69c57f9"
       sheet="6fe6d2e2-43de-4c67-8aba-6bd99ad3ba74"
       title="Famílias beneficiárias do programa de Auxílio Aluguel (dez/2024)"
       identity="8"
+      download-link="/dados/urbanismo/familias-beneficiarias-do-programa-de-auxilio-aluguel.zip"
     />
     <DashboardFrame
       appid="f571d56a-108d-4a83-b865-45255c72b23d"
       sheet="5548d5e2-5953-4beb-aaea-1345aae8f5d1"
       title="Realização Financeira em políticas de Acesso à Moradia Adequada  &mdash; Plano Plurianual 2022-2025"
       identity="10"
+      download-link="/dados/urbanismo/Quadro 7 - Relatório de Função Habitação.xlsx"
     />
     <DashboardFrame
       appid="18956ab0-d917-475f-8c94-aba437c955fc"
       sheet="SHsCbm"
       title="Despesas liquidadas em toda a função Habitação (2024)"
       identity="11"
+      download-link="/dados/urbanismo/orcamento-habitacao.csv"
     />
   </SnapPage>
 </template>
