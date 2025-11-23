6c8874343c5733c960b76b6755cd96ffc40adbeb
brunoazuma <41346451+brunoazuma@users.noreply.github.com>
2025-11-23 20:48:12 +0000

    Revert "Adicionado arquivo NE2 trabalho"
    This reverts commit 8764dfab9d405fe458f87f74daae38439909306b.

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true, // Necessário para acesso fora do container
-  },
   plugins: [vue()],
   resolve: {
     alias: {
