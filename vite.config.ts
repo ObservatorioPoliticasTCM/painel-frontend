8764dfab9d405fe458f87f74daae38439909306b
brunoazuma <41346451+brunoazuma@users.noreply.github.com>
2025-11-23 20:45:34 +0000

    Adicionado arquivo NE2 trabalho

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // Necessário para acesso fora do container
+  },
   plugins: [vue()],
   resolve: {
     alias: {
